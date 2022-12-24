3310c8188869d0d56624c25adaa5f4709075422f
Tokarev Artem <tokarev28.art@gmail.com>
2022-12-24 10:28:46 +0500

    improve tests

--- tests/initon.rs
+++ tests/initon.rs
@@ -1,18 +1,22 @@
+use std::fmt::Display;
+
 use initon::Singleton;
 
 #[tokio::test]
-async fn create_init() {
-    static SINGLETON: Singleton<Vec<i32>> = Singleton::uninitialized();
-    let life = SINGLETON.initialize(vec![0, 1]).await.unwrap();
+async fn create_uninit_then_init() {
+    static SINGLETON: Singleton<i32> = Singleton::uninitialized();
+    let life = SINGLETON.initialize(42).await.unwrap();
     assert!(SINGLETON.is_initialized().await);
+    assert_eq!(*SINGLETON.read().await.unwrap(), 42);
     drop(life);
     assert!(!SINGLETON.is_initialized().await);
 }
 
 #[tokio::test]
-async fn create_with() {
-    let singleton = Singleton::initialized(vec![0, 1]);
+async fn create_init() {
+    let singleton = Singleton::initialized(42);
     assert!(singleton.is_initialized().await);
+    assert_eq!(*singleton.read().await.unwrap(), 42);
     let life = singleton.lifetime_guard().await.unwrap();
     drop(life);
     assert!(!singleton.is_initialized().await);
@@ -29,4 +33,14 @@ async fn freed_resources() {
     }
 
     let _ = Singleton::initialized(PanicOnDrop);
+}
+
+#[tokio::test]
+async fn create_with_boxed() {
+    let singleton: Singleton<Box<dyn Display + Send + Sync>> = Singleton::uninitialized();
+    let life = singleton.initialize(Box::new(42)).await.unwrap();
+    assert!(singleton.is_initialized().await);
+    assert_eq!(&singleton.read().await.unwrap().to_string(), "42");
+    drop(life);
+    assert!(!singleton.is_initialized().await);
 }
